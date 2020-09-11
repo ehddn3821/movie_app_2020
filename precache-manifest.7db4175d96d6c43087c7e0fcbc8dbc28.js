7f2087f3007ec523779f9b31cd0c8073483bc2e7
dongwoo <dongwoo@dongwooui-MacBookPro.local>
2020-09-11 20:33:12 +0900

    Updates

---
+++ precache-manifest.7db4175d96d6c43087c7e0fcbc8dbc28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed7c699549b0aa7558a2e1ed66bd978",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "dab52fa2631317cce164",
+    "url": "/movie_app_2020/static/css/main.7d7934ae.chunk.css"
+  },
+  {
+    "revision": "dcbc1ad3746847d335f9",
+    "url": "/movie_app_2020/static/js/2.968ebb96.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.968ebb96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dab52fa2631317cce164",
+    "url": "/movie_app_2020/static/js/main.298e0d95.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
